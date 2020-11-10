b2a1eadbfe7729e0d432efcf1cbb3244c2e7c497
Joaquim Andrade <joaquimandrade@ua.pt>
2020-11-10 20:55:51 +0000

    alterações
    final guião 1

--- guiao1avalia/setimo.m
+++ guiao1avalia/setimo.m
@@ -3,10 +3,10 @@ p1=0.002;                                    %probabilidade de estragar peça
 p2=0.005;                                     %probabilidade de estragar peca2
 pmontagem= 0.01;                            %probabilidade de montagem                                 
 j=0;
-n=16;
-estraga=0;%varivael2
-probsimulacao= zeros(16, 1);
-for k=0 : 7
+n=8;
+estraga=0;                                  %varivael2
+probsimulacao= zeros(n, 1);
+for k=0 : n
     for f=1: N
         for i=1 :n
             if (rand <p1)|| (rand<p2)|| (rand <pmontagem)  %se algum dos objetos estiver estragado ou o processo de montagem correr mal entra no ciclo
@@ -22,4 +22,16 @@ for k=0 : 7
     j=0;
 end
 stem(probsimulacao);
-prob= probsimulacao(7) +probsimulacao(3)+probsimulacao(4)+probsimulacao(5)+probsimulacao(6);
+prob=0; % 2.b)X>=2 
+for i=3: n
+    prob = prob+probsimulacao(i);
+end 
+valoresperado=0; %valor esperado
+for i=0: n
+    valoresperado= probsimulacao(i+1)*i+valoresperado;
+end
+variancia=0; %variancia
+for i=0: n
+    variancia= variancia+ ((i-valoresperado)^2 * probsimulacao(i+1)) ;
+end
+desviopadrao= variancia^1/2;%desviopadrao
