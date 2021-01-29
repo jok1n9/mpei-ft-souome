d218bda46701e54a00d0bd72a288e2df01f61855
jok1n9 <joaquimandrade@ua.pt>
2021-01-29 20:39:15 +0000

    well i tried
    tried my best

--- guiao3avalia/app.m
+++ guiao3avalia/app.m
@@ -1,3 +1,4 @@
+%%lê os dados
 d= readtable("ola.txt");% lê a tabela previamente escrita
 moviesgenre= d{:,2:20};% retira 
 movies= d{:,1};         % nomes dos filmes
@@ -12,8 +13,13 @@ for n=1: Nid
     
     
 end
-minhash= d{:,userend+1:end};
+minhash= d{:,userend+1:end-2};
+a= d{1:1000, end-1};
+c= d{1:1000, end};
 minhash= minhash';
+
+
+%%começa a app
 prompt= "Insert User ID (1 to 943):";
 x= input(prompt);
 while(x <1 || x >943)               %espera por um input dentro do número de utilizadores
@@ -42,7 +48,12 @@ while y~=4          %enquanto o input 4 não aparecer não acaba
     elseif y==2 %dá sugestões do utilizador mais parecido sobre o tema inputed
         prompt= "1- Action, 2- Adventure, 3- Animation, 4- Children’s\n5- Comedy, 6- Crime, 7- Documentary, 8- Drama\n9- Fantasy, 10- Film-Noir, 11- Horror, 12- Musical\n13- Mystery, 14- Romance, 15- Sci-Fi, 16- Thriller\n17- War, 18- Western\nSelect choice:";
         k= input(prompt);
-
+        while(k <1 || x >18)               %espera por um input dentro do número de utilizadores
+            fprintf("\nOut of bounds\n");
+            prompt= "Try again:";
+        
+            k= input(prompt);
+        end
         sim=zeros( 1,length(t(1,:)));
         for i=1:length(t(1,:))
             sim(1,i)=sum(t(:,k)==t(:,i))/length(t(:,1));    
@@ -58,77 +69,55 @@ while y~=4          %enquanto o input 4 não aparecer não acaba
             end
         end
     elseif y==3 %search engine de acordo com o input
-        prompt= "\nEnter title:";
+        prompt= "\nSearch title :";
         title= input(prompt, 's');
-        s = searchMusic(title, minhash);
+        [s] = searchfilmes(title, minhash, a , c);
         if (~isempty(s(1,:)))
-            fprintf('\nMúsicas com nome mais parecido: ');
-            for i=1:length(s(1,:))
-               fprintf('\nÍndice: %d; Nome: %s',s(1,i),pathToName(musics{s(1,i)}));
+            disp("\nFilmes com nome mais parecido: ");
+            for j=1:length(s(1,:))
+                disp(movies1(1,s(1,j)));
             end
         else
-            fprintf('\nNão foram encontradas músicas com nome similar.');
+            disp("Filme não encontrado");
         end
     end
     prompt="\n1 - Your Movies\n2 - Get Suggestions\n3 - Search Title\n4 - Exit Select\n choice:";
     y= input(prompt);
 end    
 disp("end");
-% Diogo Ferreira e Fábio Cunha, 2015
-function [ most ] = searchMusic( t, minh)
-    % Recebe uma string com o nome da música a pesquisar e a table de
-    % minHash e retorna array com os índices das músicas mais parecidas e o seu valor.
-    nhash=1000;
-    p=10000019;
-	% NOTA: Threshold deve ser aumentado para set's +-maiores do que 2000!
-    threshold=0.2;
-    a=randi([2,p-2],1,nhash);
-    c=randi([2,p-1],1,nhash);
+
+function [ notw] = searchfilmes( t, minh, a , c)
+ 
+    threshold=0.8;
     
-    sim=minHashSingleDist(minh, musicSingleMinHash(t, a, c));
-    most=zeros(2,length(find(sim>=threshold)));
+    [minh2]=filmeSingleMinHash(t, a, c);
+    sim=minHashSingleDist(minh, minh2 );
+    
+    notw=zeros(2,length(find(sim>=threshold)));
     for i=1:length(find(sim>=0.2))
         m=find(sim==max(sim));
-        most(1,i)=m(1,1);
-        most(2,i)=max(sim);
-        sim(most(1,i))=0;
+        notw(1,i)=m(1,1);
+        notw(2,i)=max(sim);
+        sim(notw(1,i))=0;
     end
-    
-    
 end
-% Diogo Ferreira e Fábio Cunha, 2015
-function [ minh ] = musicSingleMinHash( str,a,c )
-    
-    % Função que calcula minHash apenas para uma String, de acordo com os
-    % valores para geração de hash recebidos.
-    
-    % Separa string de entrada em Shingles
-    shingles=strsplit(str,{' ','-','.','','(',')'});
-    j=1;
+
+
+function [ minh] = filmeSingleMinHash( str,a,c )
     shinglesname=cell(1,1);
-    % Cada shingle é colocado na cell shinglesname.
-    % Shinglesname é criado como uma cell 1x1, pois não se sabe à partida qual vai ser o seu
-    % tamanho. Como é do tipo cell, não se pode fazer operações
-    % vetoriais, pelo que trabalhar com for's pode ficar bastante lento neste caso.
-    for k=1:length(shingles(1,:))
-        if not(isequal(shingles(1,k),cellstr('mp3')) || isempty(shingles{1,k}))
-            shinglesname{j,1}=shingles{1,k};
-            j=j+1;
-        end
+    k=2;
+    l=1;
+    while(l<length(str)-k)
+            shinglesname{l,1}= extractBetween(str,l,l+k);
+            l=l+1;
     end
-    
-    % Calcula as hash para os shingles.
     k=1000;
     hash=zeros(length(shinglesname),k);
-    % Número primo>10^7
     p=10000019;
     for j=1:length(shinglesname)
-        hash(j,:)=mod(a(1,:)*sum(double(lower(shinglesname{j,1})))+c(1,:),p);
+        hash(j,:)=mod(a(1,:)*sum(double(lower(shinglesname{j,1}{:})))+c(1,:),p);
     end
-    
-    % Cria minHash Table.
     minh=Inf(k,1);
-    
     for i=1:k
         for j=1:length(shinglesname)
             minh(i,1)=min(hash(j,i), minh(i,1));
@@ -136,14 +125,10 @@ function [ minh ] = musicSingleMinHash( str,a,c )
     end
     
 end
-% Diogo Ferreira e Fábio Cunha, 2015
+
 function [ sim ] = minHashSingleDist( minh, minh2 )
-    
-    % Calcula distância baseada no minHash de um set e no minHash de uma
-    % string.
-    sim=zeros(length(minh(1,:)),1);
     for i=1:length(minh(1,:))
-        sim(i,1)=sum(minh(:,i)==minh2(:,1))/length(minh(:,1));
+        sim(1,i)=sum(minh(:,i)==minh2(:,1))/length(minh(:,1)); 
     end
 
 end
--- guiao3avalia/writing.m
+++ guiao3avalia/writing.m
@@ -13,22 +13,57 @@ movies = unique(us(:,2));
 id= unique(us(:,1));
 Nid= length(id); 
 Set= cell(height(d),1);
-
+nhash=1000;
 for n=1:Nid
     ind= find(us(:,1)== id(n));
     Set{n} = [Set{n} us(ind, 2)];
 end
-%[minh, a, c]= musicMinHash(movies1, 1000);
+[minh, hash,a, c]= filmesMinHash(movies1, 1000);
 min= minHash(id, movies, Set, height(d));
-[ minh, a, c ]=musicMinHash(movies1, 1000);
 
 
-d.min= min; 
-d.set= Set;
-d.minh= minh;
-writetable(d, "ola.txt");
 
+function [minh,hash, a, c]= filmesMinHash(filmes, nhash)
+    j=1;
 
+    shinglesl=cell(length(filmes),50);
+    shingles=cell(1,1);
+    x=0;
+    for i=1:length(filmes)
+        k=2;
+        l=1;
+       
+        while(l<length(filmes{1,i})-k)
+            shingles{i,l}= extractBetween(filmes{1,i},l,l+k);
+            %shingleslength{i,l}=j;
+            l=l+1;
+            j=j+1;
+        end
+    end 
+
+    p=10000019;
+    hash=zeros(length(shingles),nhash);
+    a=randi([2,p-2],1,1682);
+    c=randi([2,p-1],1,1682);
+    
+
+    for j=1:length(shingles)
+        hash(j,:)=mod(a(1,1:nhash)*sum(double(lower(shingles{j,1}{1})))+c(1,1:nhash),p);
+    end
+
+    minh=Inf(length(filmes),nhash);
+    l=1;
+
+    for j=1:length(filmes)
+        k=1;
+        while k<max(sum(cellfun('length',shinglesl),2)) && not(isempty(shinglesl{j,k}))
+            minh(j,:)=min(hash(shinglesl{j,k},:), minh(j,:));
+            k=k+1;
+            l=l+1;
+         end
+    end
+
+end
 function [ minh ] = minHash(users, movies, Set, size)
 
     
@@ -49,131 +84,5 @@ function [ minh ] = minHash(users, movies, Set, size)
             minh(i,u)=min(hash(Set{users(u)},i));
         end
     end
-end
-function [ minh, a, c ] = musicMinHash( musics, nhash )
-    % Função que calcula e devolve a tabela de minHash.
-    % Função otimizada (número de for's significativamente diminuído).
-    % No entanto, ainda é algo lenta.
-    
-    if nargin==1
-        nhash=100;
-    end
-    
-
-    shingles=cell(1,length(musics));
-    
-    % Variável de controlo para armazenar numa cell a posição de cada
-    % shingle
-    j=1;
-    % "Greedy" Cell para guardar posições do shingle de cada música.
-    % 50 é tamanho default. Se precisar, é aumentada automaticamente.
-    shingleslength=cell(length(musics),50);
-    shinglesname=cell(1,1);
-    
-    h=waitbar(0,'A criar shingles...');
-    x=0;
-    for i=1:length(musics)
-        waitbar(i/length(musics), h)
-    
-        if ischar(musics{1,i})
-            name=strsplit(musics{1,i},'\');
-            % Divide o nome de cada música em shingles.
-            shingles{:,i}=strsplit(name{length(strsplit(musics{1,i},'\'))},{' ','-','.','','(',')','_'});
-            
-            % Cada shingle é colocado na cell shinglesname, e a sua posição
-            % é armazenada em shingleslength.
-            % Shinglesname e Shingleslength são do tipo cell, logo não se pode fazer operações
-            % vetoriais, pelo que trabalhar com for's pode ficar bastante lento neste caso.
-            for k=1:length(shingles{:,i})
-                % Elimina todos os .mp3, ou se a cell estiver vazia.
-                if not(isequal(shingles{1,i}(k),cellstr('mp3')) || isempty(shingles{1,i}(k)))
-                    % Procura nos shingles pelo shingle novo. Caso
-                    % não exista, cria-o. Caso contrário, apenas guarda rm
-                    % shigleslength a sua posição.
-                    % ATUALIZAÇÃO : Eliminado um for, para procurar na
-                    % cell, agora feito com cálculo vetorial. Ainda assim,
-                    % este processo atrasa bastante o tempo para calcular a
-                    % minHash, apesar de libertar espaço de memória e de ser mais preciso.
-                    % Caso esteja a trabalhar com bases de dados pequenas, ou necessite de mais precisão,
-                    % pode "descomentar" abaixo e comentar a parte seguinte. Neste
-                    % caso, não haverá shingles repetidos.
-                    
-                    % Retire os comentários desde aqui.
-                    %
-                    % if iscell([shinglesname{:}])
-                    %     ind=find(strcmp([shinglesname{:}], shingles{1,i}{k}));
-                    %     if isempty(ind)
-                    %         shinglesname{j-x,1}=shingles{1,i}(k);
-                    %         shingleslength{i,k}=j-x;
-                    %     else
-                    %         shingleslength{i,k}=ind;
-                    %         x=x+1;
-                    %     end
-                    % else
-                    %     shinglesname{j-x,1}=shingles{1,i}(k);
-                    %     shingleslength{i,k}=j-x;
-                    % end
-                    
-                    % Retire os comentários até aqui.
-                    % Comente desde aqui.
-                    
-                    shinglesname{j,1}=shingles{1,i}(k);
-                    shingleslength{i,k}=j;
-                    j=j+1;
-                    % Comente até aqui.
-                end
-            end
-        end
-    end
-    delete(h)
-    
-    h=waitbar(0,'A criar hashcodes...');
-    
-   
-    
-    % Número primo>10^7
-    p=10000019;
-    hash=zeros(length(shinglesname),nhash);
-    
-    % Parâmetros das funções de hash.
-    % Serão posteriormente devolvidos para outras funções que precisem de
-    % gerar minHash.
-    a=randi([2,p-2],1,nhash);
-    c=randi([2,p-1],1,nhash);
-    
-    k=0;
-    
-    % ATUALIZAÇÃO: Eliminado um for para os hashcodes.
-    for j=1:length(shinglesname)
-        k=k+1;
-        waitbar(k/length(shinglesname), h)
-        hash(j,:)=mod(a(1,:)*sum(double(lower(shinglesname{j,1}{1})))+c(1,:),p);
-    end
-
-    delete(h);
-    
-
-    
-    h=waitbar(0,'A calcular minHash...');
-    
-    step=0;
-    % Cria matriz de "infinito".
-    minh=Inf(length(musics),nhash);
-    l=1;
-    
-    % ATUALIZAÇÃO: Eliminado for para hashcodes.
-    for j=1:length(musics)
-        step=step+1;
-        waitbar(step/(length(musics)), h)
-        k=1;
-        % K vai de 1 até ao fim da linha, que pode conter matrizes vazias.
-        while k<max(sum(cellfun('length',shingleslength),2)) && not(isempty(shingleslength{j,k}))
-            minh(j,:)=min(hash(shingleslength{j,k},:), minh(j,:));
-            k=k+1;
-            l=l+1;
-         end
-    end
-    
-    delete(h)
 end
 
